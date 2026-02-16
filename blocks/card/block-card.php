834b093fe96b3c8c183013e0c1797664d9d43e65
Mathilde Arconte <signup@marconte.fr>
2026-02-16 16:48:40 +0100

    refactor: simplify card block structure and introduce color-based styling variants.

--- blocks/card/block-card.php
+++ blocks/card/block-card.php
@@ -38,83 +38,23 @@ if ($is_preview && empty($title)) {
 }
 ?>
 
-<?php if ($style === 'showmore'): ?>
-	<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-		<div class="card-wrapper">
-			<button class="card-button" aria-label="Expand content"><i class="fa fa-plus"></i></button>
-			<div class="card-header <?php if ($image):  ?>has-image<?php endif; ?>">
-				<?php if ($image): ?>
-					<div class="card-image">
-						<img src="<?php echo esc_url($image['url']); ?>"
-							alt="<?php echo esc_attr($image['alt']); ?>"
-							loading="lazy">
-					</div>
-				<?php endif; ?>
-				<?php if ($title): ?>
-					<h4 class="card-title"><?php echo wp_kses_post($title); ?></h4>
-				<?php endif; ?>
-			</div>
+<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> card-block-<?php echo esc_attr($style); ?>">
 
-			<div class="card-content">
-				<?php if ($excerpt) : ?>
-					<div class="card-description">
-						<?php echo wp_kses_post($excerpt); ?>
-					</div>
-				<?php endif; ?>
-			</div>
-
+	<?php if ($image): ?>
+		<div class="card-image">
+			<img src="<?php echo esc_url($image['url']); ?>"
+				alt="<?php echo esc_attr($image['alt']); ?>"
+				class="no-animate"
+				loading="lazy">
 		</div>
-		<?php if (!$is_preview): ?>
-			<div class="card-modal" id="<?php echo esc_attr($id); ?>-modal" role="dialog" aria-modal="true" aria-labelledby="<?php echo esc_attr($id); ?>-modal-title">
-				<div class="card-modal-content">
-					<div class="card-header">
-						<?php if ($image): ?>
-							<div class="card-image">
-								<img src="<?php echo esc_url($image['url']); ?>"
-									alt="<?php echo esc_attr($image['alt']); ?>"
-									loading="lazy">
-							</div>
-						<?php endif; ?>
-						<?php if ($title): ?>
-							<h4 class="card-title"><?php echo wp_kses_post($title); ?></h4>
-							<button class="modal-close" aria-label="Close content"><i class="fa fa-times"></i></button>
-						<?php endif; ?>
-					</div>
-					<div class="card-content">
-						<?php if ($description): ?>
-							<div class="card-description">
-								<?php echo wp_kses_post($description); ?>
-							</div>
-						<?php endif; ?>
-					</div>
-				</div>
-			</div>
-	</div>
-<?php endif; ?>
-<?php else: ?>
-	<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-		<div class="card-wrapper">
-			<div class="card-header">
-				<?php if ($image): ?>
-					<div class="card-image">
-						<img src="<?php echo esc_url($image['url']); ?>"
-							alt="<?php echo esc_attr($image['alt']); ?>"
-							loading="lazy">
-					</div>
-				<?php endif; ?>
-				<?php if ($title): ?>
-					<h4 class="card-title"><?php echo wp_kses_post($title); ?></h4>
-				<?php endif; ?>
-			</div>
-
-			<div class="card-content">
-				<?php if ($description): ?>
-					<div class="card-description">
-						<?php echo wp_kses_post($description); ?>
-					</div>
-				<?php endif; ?>
-			</div>
-
+	<?php endif; ?>
+	<?php if ($title): ?>
+		<h4 class="card-title"><?php echo wp_kses_post($title); ?></h4>
+	<?php endif; ?>
+	<?php if ($description): ?>
+		<div class="card-description">
+			<?php echo wp_kses_post($description); ?>
 		</div>
-	</div>
-<?php endif; ?>
+	<?php endif; ?>
+
+</div>
