68e17eb88bbb704cec5fd4117a3cb59f1433bc20
Mathilde Arconte <signup@marconte.fr>
2026-02-28 12:39:14 +0100

    style: Refine button and input field styles, and update the contact page layout.

--- page-contact.php
+++ page-contact.php
@@ -11,17 +11,9 @@ get_header();
 $subtitle = get_field('subtitle');
 ?>
 
-<div class="page page_contact">
-	<?php
-	get_template_part('template-parts/page-header', null, array(
-		'subtitle' => $subtitle,
-		'thumbnail_position' => 'right',
-		'col_left' => 'col-md-8',
-		'col_right' => 'col-md-4',
-		'buttons' => false
-	));
-	?>
+<main class="site-main">
 	<div class="container">
 		<?php the_content(); ?>
 	</div>
-	<?php get_footer(); ?>
+</main>
+<?php get_footer(); ?>
